2c66b42802321542533dcc68fe2dfe24f194a728
trencoret <trencoret@uc.cl>
2017-03-14 18:03:39 -0300

    Before Comments

--- ENOL/db/schema.rb
+++ ENOL/db/schema.rb
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20170314181603) do
   create_table "comments", force: :cascade do |t|
     t.string   "name"
     t.text     "body"
+    t.integer "article_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
